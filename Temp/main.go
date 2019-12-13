ccdf786f4024eddd14533dd14a705972a59bfbc9
Viral Patel <viral.patel@fourth.com>
2019-12-13 20:34:29 +0000

    adding temp

--- Temp/main.go
+++ Temp/main.go
@@ -1,1 +1,12 @@
-package Temp
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+func main() {
+	tpl := template.Must(template.New("something").Parse("here is the test in the template"))
+	tpl.ExecuteTemplate(os.Stdout, "something", nil)
+
+}
