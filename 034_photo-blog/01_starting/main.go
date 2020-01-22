f88b1fc36c57a175907f59fca05e1ef66ab6a27e
Viral Patel <viral.patel@fourth.com>
2020-01-22 21:26:38 +0000

    adding expire to my photo-blog

--- 034_photo-blog/01_starting/main.go
+++ 034_photo-blog/01_starting/main.go
@@ -20,6 +20,7 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/expire", expire)
 	// add route to server pictures
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
 	http.Handle("/favicon.ico", http.NotFoundHandler())
@@ -84,4 +85,15 @@ func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cook
 	c.Value = s
 	http.SetCookie(w, c)
 	return c
+}
+
+func expire(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("session")
+	if err != nil {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	c.MaxAge = 30
+	http.SetCookie(w, c)
+	http.Redirect(w, req, "/", http.StatusSeeOther)
 }
