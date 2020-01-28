1a073c354e61c98585dc81adaab61f007c12ebf2
Viral Patel <viral.patel@fourth.com>
2020-01-28 20:54:09 +0000

    completing json hands on exercise

--- 040_json/16_hands-on/main.go
+++ 040_json/16_hands-on/main.go
@@ -1,14 +1,29 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
 type code struct {
-	Code    string `json: "Code"`
+	Code    int    `json: "Code"`
 	Descrip string `json: Descrip`
 }
 
 type codes []code
 
 func main() {
-	//var data codes
+	var data codes
+	rcvd := `[{"Code": 200,"Descrip": "StatusOk"},{"Code": 301, "Descrip": "StatusMovedPermanently"},{"Code": 302, "Descrip": "StatusNotFound"},{"Code": 303, "Descrip": "StatusSeeOther"},{"Code": 307, "Descrip": "StatusTempporaryRedirect"},{"Code": 400, "Descrip": "StatusBadRequest"},{"Code": 401, "Descrip": "StatusUnauthorized"},{"Code": 402, "Descrip": "StatusPaymentRequired"},{"Code": 403, "Descrip": "StatusForbidden"},{"Code": 404, "Descrip": "StatusNotFound"},{"Code": 405, "Descrip": "StatusMethodNotAllowed"},{"Code": 418, "Descrip": "StatusTeaPot"},{"Code": 500, "Descrip": "StatusInternalServerError"}]`
 
-	//rcvd :=
+	err := json.Unmarshal([]byte(rcvd), &data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, v := range data {
+		fmt.Println(v.Code, "-", v.Descrip)
+	}
+
 }
